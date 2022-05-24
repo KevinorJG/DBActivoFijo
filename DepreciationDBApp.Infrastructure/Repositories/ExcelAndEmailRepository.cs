5dfae172f71fb00ea71b9c5adcad68258969db15
Kevin-Mendoza07 <kevingfx71@gmail.com>
2022-05-24 08:28:46 -0600

    Mejoras en la creación de hoja de excel

--- DepreciationDBApp.Infrastructure/Repositories/ExcelAndEmailRepository.cs
+++ DepreciationDBApp.Infrastructure/Repositories/ExcelAndEmailRepository.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SpreadsheetLight;
 using EASendMail;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace DepreciationDBApp.Infrastructure.Repositories
 {
@@ -25,39 +26,64 @@ namespace DepreciationDBApp.Infrastructure.Repositories
                 string path = Path.GetFullPath("myexcel.xlsx");
                 SLDocument document = new SLDocument();
                 SLStyle style = new SLStyle();
-                SLStyle rowHeaderStyle = new SLStyle();
-                rowHeaderStyle.SetVerticalAlignment(DocumentFormat.OpenXml.Spreadsheet.VerticalAlignmentValues.Center);
-                rowHeaderStyle.SetHorizontalAlignment(DocumentFormat.OpenXml.Spreadsheet.HorizontalAlignmentValues.Left);
+                SLStyle ColumnsAsset = new SLStyle();
+                SLStyle RowAsset = new SLStyle();
+                
 
                 System.Data.DataTable dt = new System.Data.DataTable();
                 
-                //Columnas
+                //Columnas y estilos
                 document.SetCellValue("B2", "Nombre del Activo");
                 document.SetCellValue("C2", "Nombre del Empleado");
                 document.SetCellValue("D2", "Código del activo");
                 document.SetCellValue("E2", "DNI del empleado");
+
+                ColumnsAsset.Font.Bold = true;
+                ColumnsAsset.Fill.SetPattern(PatternValues.Solid, System.Drawing.Color.Yellow, System.Drawing.Color.Black);//Color de las columnas en B
+                document.SetCellStyle("B6", ColumnsAsset);
+                document.SetCellStyle("B8", ColumnsAsset);
+                document.SetCellStyle("B9", ColumnsAsset);
+                document.SetCellStyle("B10", ColumnsAsset);
+
+                document.SetCellValue("B6", "Descripción del Activo");
+                document.SetCellValue("B8", "Valor del activo");
+                document.SetCellValue("B9", "Vida util");
+                document.SetCellValue("B10", "Descripción");
+
                 //Filas
+                RowAsset.Alignment.Horizontal = HorizontalAlignmentValues.Center; //Centrado de las filas
+                document.SetCellValue("C8", activoEmpleado.Activo.ValorActivo);
+                document.SetCellValue("C9", activoEmpleado.Activo.VidaUtil);
+                document.SetCellValue("C10", activoEmpleado.Activo.Descripcion);
+               
+
                 document.SetCellValue("B3",activoEmpleado.Activo.Nombre);
                 document.SetCellValue("C3",activoEmpleado.Empleado.Nombre);
                 document.SetCellValue("D3",activoEmpleado.Activo.Codigo);
                 document.SetCellValue("E3",activoEmpleado.Empleado.Dni);
 
-                document.SetRowStyle(2, rowHeaderStyle);
-                //Estilo de las celdas 
-                style.Font.Bold = true;
-                style.Font.FontSize = 16;
-                style.Border.Outline = true;
                 //tamaño de ancho de columnas
                 document.SetColumnWidth(2, 24.14);
                 document.SetColumnWidth(3, 29.00);
                 document.SetColumnWidth(4, 22.71);
                 document.SetColumnWidth(5, 23.0);
-                //
-
+                //tamaño de ancho de filas
+                document.SetRowHeight(6, 15.00);
+                //Estilo de columnas principales(Celdas)
+                style.Font.Bold = true;
+                style.Font.FontSize = 16;
+                style.Border.Outline = true;
+                style.Fill.SetPattern(PatternValues.Solid, System.Drawing.Color.Yellow, System.Drawing.Color.Black);
+                style.Alignment.Horizontal = HorizontalAlignmentValues.Center;
                 document.SetCellStyle("B2", style);
                 document.SetCellStyle("C2", style);
                 document.SetCellStyle("D2", style);
                 document.SetCellStyle("E2", style);
+
+                //Centrado de filas Columna C
+                document.SetCellStyle("C8", RowAsset);
+                document.SetCellStyle("C9", RowAsset);
+                document.SetCellStyle("C10", RowAsset);
                 document.SaveAs(path);
 
 
